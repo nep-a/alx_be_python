617b2ac3d019154c1314afdae3d8f9dc148c0720
nep-a <musalakaniwitaba@gmail.com>
2025-05-23 16:52:35 +0300

    new again

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+result = number1 + number2 
+
+print("Addition of 10 and 5 is ",result)
+
+number2 = 10
+number1 = 5
+result = number2 - number1
+print("Subtraction of 10 and 5 is ",result)
+
+number1 = 10
+number2 = 5
+result = number1 * number2
+print("Multiplication of 10 and 5 is ",result)

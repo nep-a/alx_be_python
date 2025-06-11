fc6f29add415d46a8752f737c7b5bf7910ea22e6
nep-a <musalakaniwitaba@gmail.com>
2025-06-11 13:32:08 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,13 @@
-def perform_operation(num1,num2,operation):
-    if operation == "add":
-        num1 + num2
-    elif operation == "Divide":
-        num1 / num2
-    elif operation == "multiply":
-        num1 * num2
-    elif operation == "substract":
-        num1 - num2
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
     else:
-        return "Error on operator"
+        return "Error: Invalid operation"
